11fd54f5161fe62cf75ec66fca7e9f678a94f695
jly1234 <1730873674@qq.com>
2020-10-10 09:49:18 +0800

    add dev

--- src/main/java/Git/Git/HelloWord.java
+++ src/main/java/Git/Git/HelloWord.java
@@ -1,5 +1,12 @@
 package Git.Git;
 
 public class HelloWord {
+	public static void main(String[] args)
+	{
+		System.out.println("qqqqqqqqqq");
+		System.out.println("qqqqqqqqqq1");
+		System.out.println("qqqqqqqqqq2");
+		System.out.println("qqqqqqqqqq3");
+	}
 
 }
