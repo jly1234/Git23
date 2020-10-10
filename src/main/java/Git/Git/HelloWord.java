37eddf19943f30146a71186700ef2d943ca4894f
jly1234 <1730873674@qq.com>
2020-10-10 09:41:38 +0800

    add hw

--- src/main/java/Git/Git/HelloWord.java
+++ src/main/java/Git/Git/HelloWord.java
@@ -1,5 +1,11 @@
 package Git.Git;
 
-public class HelloWord {
+public class HelloWord 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello Word");
+		System.out.println("--------------------");
+	}
 
 }
